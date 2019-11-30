48e5fa729bc14fefb75d9f56f567f8e22c2f4ce3
Ojka91 <ojkalp@gmail.com>
2019-11-30 13:59:26 +0100

    first deploy, firebase test auth

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+    
+    },
+  
+    methods: {
+        login: function () {
+            // https://firebase.google.com/docs/auth/web/google-signin
+
+            // Provider
+            var provider = new firebase.auth.GoogleAuthProvider();
+           
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+               console.log(result);
+               console.log("Succeful")
+            }).catch(function(err){
+                console.log(err);
+                console.log("Error");
+            });
+        },
+     
+    },
+    mounted: function() {
+  
+    
+    }
+  
+    
+  
+  })
